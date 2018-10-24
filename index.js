226f41b8168cde032b3754e2f18b5d918b7de2f5
simonefuoco <simone.fuoco@claimcreative.com>
2018-10-24 14:31:08 +0200

    1.0.0

---
+++ index.js
@@ -0,0 +1,45 @@
+const _ = require('lodash');
+
+const fill = (args) => {
+    let mapping = null;
+    if (typeof args.mapping === "string") {
+        mapping = JSON.parse(args.mapping);
+    } else {
+        mapping = args.mapping;
+    }
+
+    let target = {};
+
+    for (const [originPath, targetPath] of Object.entries(mapping)) {
+        let originValue = _.cloneDeepWith(_.get(args.origin, originPath));
+        _.setWith(target, targetPath, originValue);
+    }
+
+    return target;
+};
+
+/**
+ * @typedef MapperArgument
+ * @property {string|object} mapping - Plain object with origin path as key and target path as value.
+ */
+
+/**
+ * Map data from one object to another base on mapping file.
+ * @function map
+ * @public
+ * @param {MapperArgument} args - mapper arguments.
+ */
+module.exports.transform = function (args) {
+    const {Transform} = require('stream');
+    return new Transform({
+        readableObjectMode: true,
+        writeableObjectMode: true,
+        transform(chunk, encoding, callback) {
+            this.push(fill({
+                mapping: args.mapping,
+                origin: chunk
+            }));
+            callback();
+        }
+    });
+}
